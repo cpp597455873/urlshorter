4c1bc745f9e5da06e0ea8d91ebc75d4b50451022
chenpiaopiao <chenpiaopiao@lightinthebox.com>
2018-05-08 17:10:10 +0800

    这个连接池极大的加快了插入速度

--- src/main/java/com/cpp/urlshorter/shortsite/ShortUrlController.java
+++ src/main/java/com/cpp/urlshorter/shortsite/ShortUrlController.java
@@ -1,64 +1,73 @@
 package com.cpp.urlshorter.shortsite;
 
 import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.*;
 
 @RestController
 public class ShortUrlController {
-    private final String charSet = "12345mnABCD6wxyzEFGHIJKLMNOPjkl7980abcdefghiqrstuvopQRSTUVWXYZ";
-
+    private final String CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String BASE_VISIT_URL = "127.0.0.1";
     @Value("${server.port}")
     int port;
 
-    private final String BASE_VISIT_URL = "127.0.0.1";
-
-    @Resource
-    private ShortUrlRepository shortUrlRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @RequestMapping(value = "/{short_url}", method = RequestMethod.GET)
+    @Transactional
     public Object visit(@PathVariable String short_url) {
-        ShortUrlModel shortUrl = shortUrlRepository.findByShortUrl(short_url);
-        if (shortUrl == null) {
+        TypedQuery<ShortUrlModel> query = entityManager.createQuery("Select s from ShortUrlModel s where s.shortUrl=:shortUrl", ShortUrlModel.class);
+        query.setParameter("shortUrl", short_url);
+        List<ShortUrlModel> resultList = query.getResultList();
+        if (resultList == null || resultList.size() == 0) {
             return new ModelAndView("web/error.html");
         } else {
-            shortUrl.setVisitCount(shortUrl.getVisitCount() + 1);
-            shortUrl.setLatestVisitTime(new Date());
-            shortUrlRepository.save(shortUrl);
-            return new RedirectView(shortUrl.getLongUrl());
+            ShortUrlModel shortUrlModel = resultList.get(0);
+            shortUrlModel.setVisitCount(shortUrlModel.getVisitCount() + 1);
+            shortUrlModel.setLatestVisitTime(new Date());
+            entityManager.merge(shortUrlModel);
+            return new RedirectView(shortUrlModel.getLongUrl());
         }
-    }
-
-    private String getBaseUrl() {
-        return BASE_VISIT_URL + (port == 80 ? "" : ":" + port) + "/";
     }
 
     @RequestMapping(value = "/create.do", method = RequestMethod.GET)
     @ResponseBody
+    @Transactional
     public Map create(@RequestParam String url) {
         if (!url.startsWith("http")) {
             url = "http://" + url;
         }
 
+        url = url + "/" + UUID.randomUUID().toString();
 
         //检查是否存在长地址  这里使用MD5这样建立索引的时候会更快
         String md5 = DigestUtils.md5DigestAsHex(url.getBytes());
-        List<ShortUrlModel> longUrlList = shortUrlRepository.findByLongUrlMd5(md5);
-        if (longUrlList != null && longUrlList.size() > 0) {
-            return ImmutableMap.of("short_url", getBaseUrl() + longUrlList.get(0).getShortUrl(), "long_url", url);
+        TypedQuery<ShortUrlModel> query = entityManager.createQuery("Select s from ShortUrlModel s where s.longUrlMd5=:longUrlMd5", ShortUrlModel.class);
+        query.setParameter("longUrlMd5", md5);
+        List<ShortUrlModel> resultList = query.getResultList();
+        if (resultList != null && resultList.size() > 0) {
+            return ImmutableMap.of("short_url", getBaseUrl() + resultList.get(0).getShortUrl(), "long_url", url);
         }
 
 
         //检查短地址是不是存在
         String oneSite = getOneSite();
-        while (shortUrlRepository.findByShortUrl(oneSite) != null) {
+        TypedQuery<Long> queryShort = entityManager.createQuery("Select count(s) from ShortUrlModel s where s.shortUrl=:shortUrl", Long.class);
+        queryShort.setParameter("shortUrl", oneSite);
+        while (queryShort.getSingleResult() != 0) {
             oneSite = getOneSite();
+            queryShort.setParameter("shortUrl", oneSite);
         }
 
         ShortUrlModel shortUrlModel = new ShortUrlModel();
@@ -66,7 +75,7 @@ public class ShortUrlController {
         shortUrlModel.setLongUrl(url);
         shortUrlModel.setLongUrlMd5(md5);
         shortUrlModel.setCreateTime(new Date());
-        shortUrlRepository.save(shortUrlModel);
+        entityManager.persist(shortUrlModel);
 
         return ImmutableMap.of("short_url", getBaseUrl() + oneSite, "long_url", url);
     }
@@ -74,9 +83,14 @@ public class ShortUrlController {
     private String getOneSite() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 8; i++) {
-            int index = new Random().nextInt(charSet.length());
-            sb.append(charSet.charAt(index));
+            int index = new Random().nextInt(CHARSET.length());
+            sb.append(CHARSET.charAt(index));
         }
         return sb.toString();
     }
+
+    private String getBaseUrl() {
+        return BASE_VISIT_URL + (port == 80 ? "" : ":" + port) + "/";
+    }
+
 }
